b50b4d25cbad0ad2778006248cf5713935c08a77
https://github.com/new <bmcordero@abany.edu>
2016-11-05 16:44:22 -0400

    Fixes Queue Class

--- Queue.java
+++ Queue.java
@@ -35,22 +35,39 @@ public class Queue
       this.tail = newNode;
     }
     else 
-    // compare nodes & put new node in order
+   
     {
-      // start at the head of the list
-      Node current = this.head;
+       tail.setNext(newNode);
+        this.tail=newNode;
       
-      while(current != null) {
-        
-          if (current.getNext() == null) {
-          current.setNext(newNode);
-          newNode.setPrev(current);
-          this.tail = newNode;
-          break;
-        }
-        current = current.getNext();
+    
       }
     }
+  public Node dequeue() {
+    // temporary node (to return)
+    Node tmp = this.head;
+    
+    // check if there is more than one
+    // item in queue
+    if (this.head == this.tail) {
+      this.head = null;
+      this.tail = null;
+    }
+    else{
+      this.head = head.getNext();
+      // isolate tmp node before returning
+      // we wouldn't have to do this if
+      // we were returning an int instead of
+      // a Node
+      tmp.setNext(null);
+    }
+    // node return the dequeued
+    return(tmp);
+  }
+  
+  public Node peek() {
+    Node tmp = new Node(this.head.getData());
+    return(tmp);
   }
    
    public boolean isEmpty() {
@@ -58,7 +75,17 @@ public class Queue
     return this.head == null;
   
 }
+   public void deleteQueue(){
+     this.head = null;
+     this.tail = null;
+   }
+   
  public void print() {
+   if (this == null) {
+     System.out.println("null");
+   }
+   else
+   {
     // local Node variable to keep track of where we are
     // start at the head of the list
     Node current = this.head;
@@ -68,40 +95,12 @@ public class Queue
       // move current to the next node
       current = current.getNext();
     }
-    System.out.println("");
+    System.out.println("");}
   }
   
-  public void printReverse() {
-    // traverses and prints from tail
-    // local Node variable to keep track of where we are
-    // start at the tail of the list
-    Node current = this.tail;
-    // loop through until we get to end of list
-    while(current != null) {
-      current.print(); // print current node
-      // move current to the next node
-      current = current.getPrev();
-    }
-    System.out.println("");
-  }
-   public boolean deleteNode(String data) {
-    Node current = this.head;
-    while(current != null) {
-      if (current.getNext() != null && current.getNext().getData() == data) {
-        current.setNext(current.getNext().getNext());
-        current.getNext().setPrev(current);
-        return(true);
-      }
-      current = current.getNext();
-    }
-    return(false);
-  }
+ }
+   
   
- public void deleteList() {
-    this.head = null;
-    this.tail = null;
-  }
-}
  
  
  
